82129b07c3c6ab8f8f4973912fd4115e666568d3
Fernando Borretti <eudoxiahp@gmail.com>
2015-04-26 13:43:50 -0300

    Add better tests

--- t/docparser.lisp
+++ t/docparser.lisp
@@ -8,9 +8,19 @@
 (in-suite tests)
 
 (test parsing
-  (let ((out))
+  (let ((index))
     (finishes
-     (setf out (docparser:parse :docparser-test-system)))
-    (print out)))
+     (setf index (docparser:parse :docparser-test-system)))
+    ;; Test the package
+    (is
+     (equal (length (docparser::index-packages index))
+            1))
+    (let ((package-index (elt (docparser::index-packages index) 0)))
+      (is
+       (equal (docparser::package-index-name package-index)
+              "DOCPARSER-TEST-SYSTEM"))
+      (is
+       (equal (length (docparser::package-index-nodes package-index))
+              29)))))
 
 (run! 'tests)
