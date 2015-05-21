c629ade00dce2ca93e32a6f20a5e4e442269a4e7
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-21 19:57:05 -0300

    More efficient tests

--- t/docparser.lisp
+++ t/docparser.lisp
@@ -39,76 +39,98 @@
        (equal (length (docparser::package-index-nodes package-index))
               32)))))
 
-(test nodes
+(test variable-nodes
   (let* ((*index* (docparser:parse :docparser-test-system))
          (nodes (docparser::package-index-nodes
-                 (elt (docparser::index-packages *index*) 0))))
-    ;; Test that individual nodes were parsed properly
-    (let ((current-node 0))
-      ;; Variables
-      (with-test-node (node docparser:variable-node "VAR")
-        t)
-      (with-test-node (node docparser:variable-node "VAR2")
-        t)
-      (with-test-node (node docparser:variable-node "CONST")
-        t)
-      ;; The `func` function
-      (with-test-node (node docparser:function-node "FUNC")
+                 (elt (docparser::index-packages *index*) 0)))
+         (current-node 0))
+    (with-test-node (node docparser:variable-node "VAR")
+      t)
+    (with-test-node (node docparser:variable-node "VAR2")
+      t)
+    (with-test-node (node docparser:variable-node "CONST")
+      t)))
+
+(test operator-nodes
+  (let* ((*index* (docparser:parse :docparser-test-system))
+         (nodes (docparser::package-index-nodes
+                 (elt (docparser::index-packages *index*) 0)))
+         (current-node 3))
+    ;; The `func` function
+    (with-test-node (node docparser:function-node "FUNC")
+      (is
+       (equal (length (docparser:operator-lambda-list node))
+              5)))
+    ;; The `mac` macro
+    (with-test-node (node docparser:macro-node "MAC")
+      (is
+       (equal (length (docparser:operator-lambda-list node))
+              3)))))
+
+(test type-nodes
+  (let* ((*index* (docparser:parse :docparser-test-system))
+         (nodes (docparser::package-index-nodes
+                 (elt (docparser::index-packages *index*) 0)))
+         (current-node 5))
+    ;; The `rec1` struct
+    (with-test-node (node docparser:struct-node "REC1"))
+                                        ; Skip some defstruct-generated stuff
+    (incf current-node 9)
+    ;; The `rec2` struct
+    (with-test-node (node docparser:struct-node "REC2"))
+                                        ; Skip some defstruct-generated stuff
+    (incf current-node 7)
+    ;; The `custom-string` type
+    (with-test-node (node docparser:type-node "CUSTOM-STRING"))
+    ;; The `test-class` class
+    (with-test-node (node docparser:class-node "TEST-CLASS")
+      (is (equal (length (docparser:record-slots node))
+                 3))
+      (let ((first-slot (first (docparser:record-slots node))))
         (is
-         (equal (length (docparser:operator-lambda-list node))
-                5)))
-      ;; The `mac` macro
-      (with-test-node (node docparser:macro-node "MAC")
+         (typep first-slot 'docparser:class-slot-node))
         (is
-         (equal (length (docparser:operator-lambda-list node))
-                3)))
-      ;; The `rec1` struct
-      (with-test-node (node docparser:struct-node "REC1"))
-      ; Skip some defstruct-generated stuff
-      (incf current-node 9)
-      ;; The `rec2` struct
-      (with-test-node (node docparser:struct-node "REC2"))
-      ; Skip some defstruct-generated stuff
-      (incf current-node 7)
-      ;; The `custom-string` type
-      (with-test-node (node docparser:type-node "CUSTOM-STRING"))
-      ;; The `test-class` class
-      (with-test-node (node docparser:class-node "TEST-CLASS")
-        (is (equal (length (docparser:record-slots node))
-                   3))
-        (let ((first-slot (first (docparser:record-slots node))))
-          (is
-           (typep first-slot 'docparser:class-slot-node))
-          (is
-           (equal (symbol-name (docparser:node-name first-slot))
-                  "FIRST-SLOT"))
-          (is
-           (equal (docparser:node-docstring first-slot)
-                  "docstring"))))
-      ;; The `test-method` defgeneric
-      (incf current-node)
-      ;; The `test-method` method
-      (incf current-node)
-      ;; The `indirectly-define-function` macro
-      (with-test-node (node docparser:macro-node "INDIRECTLY-DEFINE-FUNCTION")
+         (equal (symbol-name (docparser:node-name first-slot))
+                "FIRST-SLOT"))
         (is
-         (equal (length (docparser:operator-lambda-list node))
-                0)))
-      ;; The `hidden-function` function
-      (with-test-node (node docparser:function-node "HIDDEN-FUNCTION")
-        (is
-         (equal (length (docparser:operator-lambda-list node))
-                0)))
-      ;; The `size-t` CFFI type
-      (with-test-node (node docparser:cffi-type "SIZE-T"))
-      ;; The `nums` CFFI enum
-      (with-test-node (node docparser:cffi-enum "NUMS")
-        (is (equal (docparser:cffi-enum-variants node)
-                   (list :a :b :c))))
-      ;; The `bits` CFFI bitfield
-      (with-test-node (node docparser:cffi-bitfield "BITS")
-        (is (equal (docparser:cffi-bitfield-masks node)
-                   (list :a :b :c)))))))
+         (equal (docparser:node-docstring first-slot)
+                "docstring"))))))
+
+(test method-nodes
+  (let* ((*index* (docparser:parse :docparser-test-system))
+         (nodes (docparser::package-index-nodes
+                 (elt (docparser::index-packages *index*) 0)))
+         (current-node 25))
+    ;; The `test-method` defgeneric
+    (incf current-node)
+    ;; The `test-method` method
+    (incf current-node)
+    ;; The `indirectly-define-function` macro
+    (with-test-node (node docparser:macro-node "INDIRECTLY-DEFINE-FUNCTION")
+      (is
+       (equal (length (docparser:operator-lambda-list node))
+              0)))
+    ;; The `hidden-function` function
+    (with-test-node (node docparser:function-node "HIDDEN-FUNCTION")
+      (is
+       (equal (length (docparser:operator-lambda-list node))
+              0)))))
+
+(test cffi-nodes
+  (let* ((*index* (docparser:parse :docparser-test-system))
+         (nodes (docparser::package-index-nodes
+                 (elt (docparser::index-packages *index*) 0)))
+         (current-node 29))
+    ;; The `size-t` CFFI type
+    (with-test-node (node docparser:cffi-type "SIZE-T"))
+    ;; The `nums` CFFI enum
+    (with-test-node (node docparser:cffi-enum "NUMS")
+      (is (equal (docparser:cffi-enum-variants node)
+                 (list :a :b :c))))
+    ;; The `bits` CFFI bitfield
+    (with-test-node (node docparser:cffi-bitfield "BITS")
+      (is (equal (docparser:cffi-bitfield-masks node)
+                 (list :a :b :c))))))
 
 (test queries
   (let ((*index* (docparser:parse :docparser-test-system)))
